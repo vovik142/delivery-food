9319e7d7c945041403581bd9806285cd649fe26a
Vova <vovik142@ukr.net>
2020-10-28 11:45:18 +0200

    second day

--- js/main.js
+++ js/main.js
@@ -1,19 +1,8 @@
+'use strict';
+
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-
-
-
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  modal.classList.toggle("is-open");
-}
-
-
-
-
 const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
@@ -21,11 +10,22 @@ const logInForm = document.querySelector('#logInForm');
 const loginInput = document.querySelector('#login');
 const userName = document.querySelector('.user-name');
 const buttonOut = document.querySelector('.button-out');
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
 
 
 console.log('loginInput');
 
 let login = localStorage.getItem('gloDelivery');
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
 
 function toggleModalAuth(){
   modalAuth.classList.toggle('is-open');
@@ -105,4 +105,115 @@ if (login) {
   notAuthorized();
 }
 }
+
+
+
+function createCartRestaurant(){
+
+  const card = `
+  <a class="card card-restaurant">
+						<img src="img/pizza-plus/preview.jpg" alt="image" class="card-image"/>
+						<div class="card-text">
+							  <div class="card-heading">
+								  <h3 class="card-title">Пицца плюс</h3>
+							  	<span class="card-tag tag">50 мин</span>
+						  	</div>
+						  	<div class="card-info">
+							  	<div class="rating">
+							  		4.5
+							  	</div>
+								<div class="price">От 900 ₽</div>
+						  	  	<div class="category">Пицца</div>
+						  	</div>
+						</div>
+	</a>
+					
+  `;
+
+  cardsRestaurants.insertAdjacentHTML('beforeend', card);
+
+
+}
+
+
+
+createCartRestaurant();
+createCartRestaurant();
+createCartRestaurant();
+
+
+function createCardGood(){
+  const card = document.createElement('div');
+
+  card.className = 'card';
+
+  card.insertAdjacentHTML('beforeend', `
+  
+ 
+  <img src="img/pizza-plus/pizza-classic.jpg" alt="image" class="card-image"/>
+  <div class="card-text">
+    <div class="card-heading">
+      <h3 class="card-title card-title-reg">Пицца Классика</h3>
+    </div>
+    <div class="card-info">
+      <div class="ingredients">Соус томатный, сыр «Моцарелла», сыр «Пармезан», ветчина, салями,
+        грибы.
+      </div>
+    </div>
+    <div class="card-buttons">
+      <button class="button button-primary button-add-cart">
+        <span class="button-card-text">В корзину</span>
+        <span class="button-cart-svg"></span>
+      </button>
+      <strong class="card-price-bold">510 ₽</strong>
+    </div>
+  </div>
+
+  `);
+
+  cardsMenu.insertAdjacentElement('beforeend', card);
+}
+
+
+
+function openGoods(event){
+  const target = event.target;
+  const restaurant = target.closest('.cards-restaurants');
+  // console.log('restaurant:', restaurant)
+  if(restaurant){
+    cardsMenu.textContent = '';
+    containerPromo.classList.add('hide');
+    restaurants.classList.add('hide');
+    menu.classList.remove('hide');
+    createCardGood();
+    createCardGood();
+    createCardGood();
+  }
+
+  openModal()
+}
+
+function openModal(){
+  if(notAuthorized){
+    modalAuth.classList.add("is-open");
+  }
+}
+ 
+cartButton.addEventListener("click", toggleModal);
+
+close.addEventListener("click", toggleModal);
+
+cardsRestaurants.addEventListener('click', openGoods);
+
+
+logo.addEventListener('click', function(){
+  containerPromo.classList.remove('hide');
+  restaurants.classList.remove('hide');
+  menu.classList.add('hide');
+}
+
+
+
+);
+
 checkAuth();
